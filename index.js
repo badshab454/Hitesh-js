bb6aafd880497b04cce653754a5e525970f408d9
Badsha Hossain <badshab454@gmail.com>
2023-10-10 11:27:13 +0600

    var let const

---
+++ index.js
@@ -0,0 +1,9 @@
+const accountId = 3;
+let accountEmail = "badsha@gmail.com";
+var accountPassword = "12345";
+accountCity = "Dhaka";
+
+accountEmail = "badboy@gmail.com";
+
+ console.log(accountEmail);
+
