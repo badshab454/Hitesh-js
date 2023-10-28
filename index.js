f639d75c92f70ad52d4766c6793816fe5f567535
Badsha Hossain <badshab454@gmail.com>
2023-10-28 15:06:15 +0600

    this arrow function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "c", 4: "d"}
+const user = {
+    username: "hitesh",
+    price: 999,
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`);
+    }
 
-const obj3 = Object.assign({}, obj1, obj2)
-console.log(obj3);
+}
+user.welcomeMessage()
