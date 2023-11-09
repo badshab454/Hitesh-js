381a969652132c5d72e3a8142fb6e63f32abfba4
Badsha Hossain <badshab454@gmail.com>
2023-11-09 12:28:04 +0600

    map method

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-myNums.filter((num) => num > 4)
+// const newNums = myNums.filter((num) => num > 4)
+
+// console.log(newNums);
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNumbers.map((num) => num + 10)
+console.log(newNums);
