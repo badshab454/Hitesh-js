9ed09e5a0a8390d560864de7c20ea259d5473f23
Badsha Hossain <badshab454@gmail.com>
2023-11-09 12:01:50 +0600

    filter array

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const coding = ["js", "ruby", "java", "python", "cpp"];
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-coding.forEach(function (val)){
-    console.log(val);
-}
+myNums.filter((num) => num > 4)
