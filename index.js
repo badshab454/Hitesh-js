56e2c91bb986d615c4474788c09843e5ea23c35b
Badsha Hossain <badshab454@gmail.com>
2023-11-12 01:27:15 +0600

    dom manipulation

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// const newNums = myNums.filter((num) => num > 4)
-
-// console.log(newNums);
-
-const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// const newNums = myNumbers.map((num) => num + 10)
-
-const newNums = myNumbers.map((num) => num *10).map((num) => num + 2).filter((num) => num >= 40)
-
-console.log(newNums);
+var ages  = [10, 23, 9, 20]
+var adultAge = 18;
+ages >= adultAge;
