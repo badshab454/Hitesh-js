bd3a273c9f7c484c9e5e11003f90fdbe08bcbca2
Badsha Hossain <badshab454@gmail.com>
2023-11-13 13:11:30 +0600

    api

--- index.js
+++
@@ -1,9 +0,0 @@
-function addLanguage(langName){
-    const li = document.createElement('li');
-    li.innerHTML = `${langName}`
-    document.querySelector('.language').appendChild(li)
-}
-addLanguage("python")
-addLanguage("java")
-
-const secondLang = document.querySelector("li:nth-child(2)")
